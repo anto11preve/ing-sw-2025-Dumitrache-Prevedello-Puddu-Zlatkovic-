0d479c56849c58db8885458f0fad9154c130a291
Alexandra <alexandradumitrachee@gmail.com>
2025-03-31 22:00:25 +0200

    added shipboard

--- src/main/java/Model/ShipBoard.java
+++ src/main/java/Model/ShipBoard.java
@@ -35,25 +35,63 @@ class ShipBoard {
     }
 
     public boolean checkIntegrity() {
-        //to do
-        return true;
+        boolean[][] visited = new boolean[5][7];
+        int total = 0;
+        int startX = -1, startY = -1;
+
+        // Trova componente iniziale e conta i totali
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 7; j++) {
+                if (components[i][j] != null) {
+                    total++;
+                    if (startX == -1) {
+                        startX = i;
+                        startY = j;
+                    }
+                }
+            }
+        }
+
+        if (total == 0) return true; // nessun modulo
+
+        int visitedCount = dfs(startX, startY, visited);
+        return visitedCount == total;
+    }
+
+    private int dfs(int x, int y, boolean[][] visited) {
+        if (x < 0 || y < 0 || x >= 5 || y >= 7) return 0;
+        if (visited[x][y] || components[x][y] == null) return 0;
+
+        visited[x][y] = true;
+        int count = 1;
+        count += dfs(x + 1, y, visited);
+        count += dfs(x - 1, y, visited);
+        count += dfs(x, y + 1, visited);
+        count += dfs(x, y - 1, visited);
+        return count;
     }
 
     public void setActiveComponent(SpaceshipComponent component) {
-        //to do
+        this.activeComponent = component;
     }
 
     public int[] getIndex(SpaceshipComponent goalComponent) {
-        //to do
-        return new int[2];
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 7; j++) {
+                if (components[i][j] == goalComponent) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{-1, -1};
     }
 
     public void reserveComponent(SpaceshipComponent component) {
-        //to do
+        reservedComponents.add(component);
     }
 
     public void removeReservedComponent(SpaceshipComponent component) {
-        //to do
+        reservedComponents.remove(component);
     }
 
     public List<SpaceshipComponent> getReservedComponents() {
@@ -63,5 +101,4 @@ class ShipBoard {
     public CondensedShip getCondensedShip() {
         return condensedShip;
     }
-
 }
