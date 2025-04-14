1f491c2fb6bb4c44d43c9eeeddd7c37bcad196f7
anto11preve <anto11preve@gmail.com>
2025-04-14 22:44:32 +0200

    Test

--- src/main/java/Model/Game.java
+++ src/main/java/Model/Game.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+
 class Game {
     private List<Player> players;
     private List<SpaceshipComponent> hiddenComponents;
