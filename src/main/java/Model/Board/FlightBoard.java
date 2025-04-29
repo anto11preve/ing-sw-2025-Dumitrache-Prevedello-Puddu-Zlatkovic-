62546f319c30a7b307cbf462d67a995261e6f28c
filzl <filip@zlatkovic.org>
2025-04-29 14:45:33 +0200

    Modified FlightBoard class
    Added JavaDoc
    Changed constructors
    Added checking for constructor arguments

--- src/main/java/Model/Board/FlightBoard.java
+++ src/main/java/Model/Board/FlightBoard.java
@@ -1,34 +1,119 @@
 package Model.Board;
 
 import Model.Board.AdventureCards.AdventureCard;
+import Model.Enums.CardLevel;
 import Model.Player;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-
-//we're missing the 4 piles of cards
+/**
+ * FlightBoard: it holds all the good stuff you
+ * need during the building and the flying phase.
+ *
+ * <p>This class would really benefit from getting
+ * split into LearnerFlightBoard and LevelTwoFlightBoard.
+ * Why? In the learner flight, specifically in the building
+ * phase, timer doesn't exist (see page 4 of
+ * {@code galaxy-trucker-rules-it.pdf}), the hiddenCardDeck
+ * is technically not formed yet (see page 9) and there
+ * is no such thing as a peekableCardDeck. Regardless of
+ * the phase, the number of cells differs, as does the layout,
+ * and it would overall be a good idea to abstract this better.
+ *
+ * <p>Also would benefit if split into FlightBoard
+ * and BuildingFlightBoard (or BuildingBoard).
+ * Why? timer, hiddenCardDeck and peekableCardDecks
+ * do not exist while flying (but this is a less pressing
+ * issue than the one stated earlier).
+ */
 public class FlightBoard {
-    Timer timer;
-    List<CardDeck> peekableCardDecks;
-    CardDeck hiddenCardDeck;
-    CardDeck upcomingCardDeck;
-    private Map<Player, Integer> playerPositions;
+    private final int cellNumber;
+
+    private Timer timer;
+
+    private List<CardDeck> peekableCardDecks;
+
+    private CardDeck hiddenCardDeck;
+
+    private CardDeck upcomingCardDeck;
+
+    private final Map<Player, Integer> playerPositions;
+
+    /**
+     * FlightBoard default constructor stub.
+     *
+     * <p>Now always throws because: How can I know if
+     * it's supposed to be a learner or a level 2
+     * flightboard? How can I make such assumption?
+     * Should I make such an assumption?
+     *
+     * @throws UnsupportedOperationException always
+     */
 
     public FlightBoard() {
-        this.timer = new Timer();
-        this.peekableCardDecks = List.of(new CardDeck(), new CardDeck(), new CardDeck());
-        this.hiddenCardDeck = new CardDeck();
-        this.upcomingCardDeck = new CardDeck();
+        throw new UnsupportedOperationException("This FlightBoard constructor is no longer accepted");
+    }
+
+    /**
+     * FlightBoard constructor of learner level.
+     * Sets the number of cells to 18.
+     * Sets the hidden card deck to the passed value.
+     * Sets the player positions map to an empty HashMap.
+     * Sets every other member to {@code null}.
+     *
+     * @param hiddenCards the deck of cards to be used later in the
+     *                    flight phase
+     * @throws AssertionError if something is wrong with passed deck:
+     *                        the number of cards is not 8, the level of the cards is not LEARNER...
+     * @see CardDeck
+     * @see AssertionError
+     */
+    public FlightBoard(AdventureCard[] hiddenCards) throws AssertionError {
+        this.cellNumber = 18;
+        this.timer = null;
+        this.peekableCardDecks = null;
+
+        this.hiddenCardDeck = new CardDeck(hiddenCards);
+
+        assert (this.hiddenCardDeck.peekCards().size() == 8);
+
+        for (AdventureCard card : this.hiddenCardDeck) {
+            assert (card.getLevel() == CardLevel.LEARNER);
+        }
+
+        this.upcomingCardDeck = null;
         this.playerPositions = new HashMap<>();
     }
 
-    public Timer getTimer() {
+    public FlightBoard(AdventureCard[] hiddenCardDeck, List<CardDeck> peekableCardDecks) {
+        this.cellNumber = 24;
+        this.timer = new Timer();
+
+        assert (3 == peekableCardDecks.size());
+        for (CardDeck cardDeck : peekableCardDecks) {
+            assert (4 == cardDeck.peekCards().size());
+        }
+        this.peekableCardDecks = peekableCardDecks;
+
+        this.hiddenCardDeck = new CardDeck(hiddenCardDeck);
+        assert (4 == this.hiddenCardDeck.peekCards().size());
+
+        this.upcomingCardDeck = null;
+        this.playerPositions = new HashMap<>();
+    }
+
+    public final int getCellNumber() {
+        return cellNumber;
+    }
+
+    public final Timer getTimer() {
         return timer;
     }
 
-    public CardDeck getPeekableCardDeck(int index) {
+    public CardDeck getPeekableCardDeck(int index) throws IndexOutOfBoundsException {
         return peekableCardDecks.get(index);
     }
 
@@ -40,22 +125,48 @@ public class FlightBoard {
         return upcomingCardDeck;
     }
 
-    public void setUpcomingCardDeck() {
+    public boolean setUpcomingCardDeck() {
+        if (timer == null || !(timer.getPhase() == Timer.Phase.LAST_PHASE && timer.getTimeLeft() == 0.0f)) {
+            return false;
+        }
+        List<AdventureCard> cards = new ArrayList<>();
+        upcomingCardDeck = new CardDeck();
         // Aggiungi le carte dei peekableCardDecks
         for (CardDeck deck : peekableCardDecks) {
-            for (AdventureCard card : deck.peekCards()) {
-                upcomingCardDeck.pushCard(card);
+            while (!deck.peekCards().isEmpty()) {
+                upcomingCardDeck.pushCard(deck.popCard());
             }
         }
 
         // Aggiungi le carte del hiddenCardDeck
-        for (AdventureCard card : hiddenCardDeck.peekCards()) {
-            upcomingCardDeck.pushCard(card);
+        while (!hiddenCardDeck.peekCards().isEmpty()) {
+            upcomingCardDeck.pushCard(hiddenCardDeck.popCard());
         }
 
+        peekableCardDecks = null;
+        hiddenCardDeck = null;
+
+        this.timer = null;
+
+        return true;
     }
 
-    //public Model.Cards.AdventureCard nextAdventure() { }
+    /**
+     * Returns the position of the specified player, or {@code null}
+     * if no position was found for such player.
+     *
+     * @param player the player whose associated position is to be returned
+     * @return the position to which the specified player is mapped, or
+     * {@code null} if this map contains no position for such player
+     * @throws NullPointerException if the specified key is null
+     */
+    public int getPosition(Player player) throws NullPointerException {
+        if (player == null) {
+            throw new NullPointerException("player is null");
+        }
+        return playerPositions.get(player);
+    }
+
     public void updatePosition(Player player, int position) {
         playerPositions.put(player, position);
     }
