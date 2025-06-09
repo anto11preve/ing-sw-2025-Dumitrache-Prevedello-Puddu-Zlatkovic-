3125d0f8ea1da22dae7938f1281aa73bcf4facef
MarcoPdd <puddumarcoo@gmail.com>
2025-06-09 10:23:37 +0200

    Ipotesi gestione eccezioni nella classe Controller

---
+++ src/main/java/Controller/Commands/LeaveRaceCommand.java
@@ -0,0 +1,32 @@
+package Controller.Commands;
+
+import Controller.Controller;
+import Controller.Exceptions.InvalidCommand;
+import Controller.Exceptions.InvalidParameters;
+
+/**
+ * Command for voluntarily leaving the race during flight phase.
+ * Players can abandon the race to limit losses.
+ */
+public class LeaveRaceCommand extends Command {
+    
+    /**
+     * Constructs a new LeaveRaceCommand.
+     *
+     * @param playerName the name of the player leaving the race
+     * @param gameID the ID of the game session
+     */
+    public LeaveRaceCommand(String playerName, int gameID) {
+        super(playerName, gameID);
+    }
+    
+    /**
+     * Executes the leave race command by calling the controller's leaveRace method.
+     *
+     * @param controller the controller to execute the command on
+     */
+    @Override
+    public void execute(Controller controller) throws InvalidCommand, InvalidParameters{
+        controller.leaveRace(getPlayerName());
+    }
+}
