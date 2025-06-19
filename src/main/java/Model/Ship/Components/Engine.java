116df14dac0f05b75cb01e438579e55230093d20
MarcoPdd <puddumarcoo@gmail.com>
2025-06-19 12:10:10 +0200

    sistemando componenti

--- src/main/java/Model/Ship/Components/Engine.java
+++ src/main/java/Model/Ship/Components/Engine.java
@@ -17,25 +17,17 @@ public class Engine extends SpaceshipComponent {
 
     private final boolean isDouble;
     private final int baseEnginePower;
-    private final int energyCost;
     private final boolean requiresBattery;
-    private final String imagePath;
-    private final String animationPath;
     private final List<String> abilities;
     private boolean hasAlien;
-    private boolean activated; //   whether the engine has been activated with a battery
 
     public Engine(Card type, ConnectorType front, ConnectorType rear, ConnectorType left, ConnectorType right, boolean isDouble) {
         super(type, front, rear, left, right);
         this.isDouble = isDouble;
         this.baseEnginePower = isDouble ? 2 : 1;
-        this.energyCost = 0;
         this.requiresBattery = isDouble;
-        this.imagePath = "";
-        this.animationPath = "";
         this.abilities = new ArrayList<>();
         this.hasAlien = false;
-        this.activated = !isDouble; // single engines are always "activated"
     }
 
     public Engine(JsonObject json) {
@@ -58,7 +50,6 @@ public class Engine extends SpaceshipComponent {
             abilities.add(e.getAsString());
         }
         this.hasAlien = false;
-        this.activated = !isDouble; // single engines default to active
     }
 
     public boolean isDoubleEngine() {
@@ -66,66 +57,10 @@ public class Engine extends SpaceshipComponent {
     }
 
     /**
-     * Activates this engine using a battery (needed for double engines).
-     */
-    public void activate() {
-        if (isDouble) {
-            this.activated = true;
-        }
-    }
-
-    /**
-     * Deactivates this engine (e.g., if battery is lost).
-     */
-    public void deactivate() {
-        if (isDouble) {
-            this.activated = false;
-        }
-    }
-
-    /**
-     * Returns whether this engine has been activated (only matters for double engines).
-     */
-    public boolean isActivated() {
-        return activated;
-    }
-
-    /**
      * Returns the base engine power (without alien bonus).
      */
     public int getEnginePower() {
         return baseEnginePower;
-    }
-
-    /**
-     * Returns the effective engine power considering activation and alien bonus.
-     * Double engines return 0 if not activated.
-     */
-    public int getEffectiveEnginePower() {
-        if (isDouble && !activated) {
-            return 0;
-        }
-        return hasAlien ? baseEnginePower + 1 : baseEnginePower;
-    }
-
-    public int getEnergyCost() {
-        return energyCost;
-    }
-
-    public boolean requiresBattery() {
-        return requiresBattery;
-    }
-
-    public String getImagePath() {
-        return imagePath;
-    }
-
-    public String getAnimationPath() {
-        return animationPath;
-    }
-
-    public List<String> getAbilities() {
-        return abilities;
     }
 
     public Direction getThrustDirection() {
